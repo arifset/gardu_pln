3469719d7ca38a1e5eff62e5d19deed164cae52d
arifset <arifunawan@gmail.com>
2016-02-23 18:13:00 +0800

    database tabel - > born trafo tak ganti tahun trafo

--- application/models/mtrafo.php
+++ application/models/mtrafo.php
@@ -2,7 +2,7 @@
 class Mtrafo extends CI_Model{
 
 	function show_trafo(){
-		$this->db->select('name_trafo,sn_trafo,kapasitas_trafo,hvnominal_trafo,lvnominal_trafo,temp_oil_trafo,born_trafo,type_trafo');
+		$this->db->select('name_trafo,sn_trafo,kapasitas_trafo,hvnominal_trafo,lvnominal_trafo,temp_oil_trafo,tahun_trafo,type_trafo');
 		$this->db->from('data_trafo');
 		return $this->db->get()->result();
 	}
