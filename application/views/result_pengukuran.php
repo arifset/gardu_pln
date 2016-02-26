c5bdba31702647eaac9743269787f7ee2f19ab50
arifset <arifunawan@gmail.com>
2016-02-26 15:18:07 +0800

    database

--- application/views/result_pengukuran.php
+++ application/views/result_pengukuran.php
@@ -14,16 +14,16 @@
           <div class="row">
             <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="info-box">
-                <span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>
+                <span class="info-box-icon bg-aqua"></i></span>
                 <div class="info-box-content">
                   <span class="info-box-text">Kondisi Visual Trafo</span>
-                  <span class="info-box-number">sdfsdfsdf sdfsesdfsdfsdf sfsdf sdfsdfsdf sfsdf s sfsdfsdfsf s sdfdsf s s sfdsdfsdfsdfsdfdsfds sfsdfs dsfsdf sfsdf sdsf sd fsdfsdf sfsd<small>%</small></span>
+                  <span class="info-box-number">sdfsdfsdf <small>%</small></span>
                 </div><!-- /.info-box-content -->
               </div><!-- /.info-box -->
             </div><!-- /.col -->
             <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="info-box">
-                <span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+                <span class="info-box-icon bg-red"></i></span>
                 <div class="info-box-content">
                   <span class="info-box-text">Kondisi Minyak Trafo</span>
                   <span class="info-box-number">asdadssf</span>
@@ -36,7 +36,7 @@
 
             <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="info-box">
-                <span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+                <span class="info-box-icon bg-green"></i></span>
                 <div class="info-box-content">
                   <span class="info-box-text">Kondisi Thermal Trafo</span>
                   <span class="info-box-number">asdsadsadadas</span>
@@ -45,7 +45,7 @@
             </div><!-- /.col -->
             <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="info-box">
-                <span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+                <span class="info-box-icon bg-yellow"></i></span>
                 <div class="info-box-content">
                   <span class="info-box-text">Kualitas Daya</span>
                   <span class="info-box-number">sdfsdfsdf</span>
@@ -54,11 +54,41 @@
             </div><!-- /.col -->
             <div class="col-md-4 col-sm-6 col-xs-12">
               <div class="info-box">
-                <span class="info-box-icon bg-red"><i class="ion ion-ios-people-outline"></i></span>
+                <span class="info-box-icon bg-red"></i></span>
                 <div class="info-box-content">
                   <span class="info-box-text">Kondisi Thermal PHBTR</span>
                   <span class="info-box-number">sdfsdfsdf</span>
                 </div><!-- /.info-box-content -->
               </div><!-- /.info-box -->
             </div><!-- /.col -->
+            
           </div><!-- /.row -->
+
+<div class="row">
+            <div class="col-md-12">
+              <div class="box">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Rekomendasi Tindakan</h3>
+                  <div class="box-tools pull-right">
+                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    <div class="btn-group">
+                      <button class="btn btn-box-tool dropdown-toggle" data-toggle="dropdown"><i class="fa fa-wrench"></i></button>
+                      <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                      </ul>
+                    </div>
+                    
+                  </div>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+                  <div class="row">
+                    <div class="col-md-8">
+                    </div><!-- /.box-footer -->
+              </div><!-- /.box -->
+            </div><!-- /.col -->
+          </div><!-- /.row -->
+          </div>
